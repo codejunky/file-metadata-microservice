5a547f35d5af0177883f23630e7d9acc8acd3a1d
Oussama Bouguerne <oussama.bouguerne@metaworks.xyz>
2016-11-23 01:49:53 +0100

    Create functionality to upload files

--- index.js
+++ index.js
@@ -1,11 +1,18 @@
 const express = require('express');
 const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 const path = require('path');
 
 const app = express();
 
-app.get('/', (res, req) => {
+app.get('/', (req, res) => {
   res.sendFile(path.resolve(__dirname, 'index.html'));
+});
+
+app.post('/upload', upload.single('file'), (req, res) => {
+  res.json({
+    size: req.file.size
+  })
 });
 
 app.listen(process.env.PORT || 3000, () => {
